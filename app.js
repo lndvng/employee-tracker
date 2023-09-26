a067f0c0f4855cb876c4e9deea57de3975fd09dc
Drinkmilk <141216114+lndvng@users.noreply.github.com>
2023-09-25 22:00:57 -0700

    added prompts for adding role and updating employee

--- app.js
+++ app.js
@@ -1,61 +1,121 @@
-const inquirer = require('inquirer');
+// import packages
+const inquirer = require("inquirer");
+const mysql = require("mysql2");
 
-const mysql = require('mysql2');
-
-// create the connection to database
+// connection to database
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'drinkmilk',
-  database: 'cms_db'
+  host: "localhost",
+  user: "root",
+  password: "drinkmilk",
+  database: "cms_db"
 });
 
+// array for user input
 const questions = [
     {
-      type: 'list',
-      name: 'userChoice',
-      message: 'What would you like to do?',
-      choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', "Add a role", "Add an employee", "Update an employee role"]
+      type: "list",
+      name: "userChoice",
+      message: "What would you like to do?",
+      choices: ["View all departments", "View all roles", "View all employees", "Add a department", "Add a role", "Add an employee", "Update an employee role"]
     },
-  ];
+];
+
+const addRole = [
+    {
+        type: "input",
+        name: "roleName",
+        message: "What is the name of the role?"
+    },
+    {
+        type: "input",
+        name: "roleSalary",
+        message: "What is the salary of the role?"
+    },
+    {
+        type: "list",
+        name: "roleDep",
+        message: "Which department does the role belong to?",
+        choices: []
+    }
+];
+
+const updateEmployee = [
+    {
+        type: "input",
+        name: "empFirstName",
+        message: "What is the employee's first name?"
+    },
+    {
+        type: "input",
+        name: "empLastName",
+        message: "What is the employee's last name?"
+    },
+    {
+        type: "list",
+        name: "empRole",
+        message: "What is the employee's role?",
+        choices: []
+    },
+    {
+        type: "list",
+        name: "empMngr",
+        message: "Who is employee's manager?",
+        choices: []
+    },
+];
   
 
 inquirer.prompt(questions)
 .then((answer) => {
-    if (answer.userChoice == 'View all departments') {
+    if (answer.userChoice == "View all departments") {
         connection.query(
-            'SELECT * FROM `department`',
+            "SELECT * FROM `department`",
             function(err, results,) {
               console.log(results);
             }
           );
     }   
-    else if (answer.userChoice == 'View all roles') {
+    else if (answer.userChoice == "View all roles") {
         connection.query(
-            'SELECT * FROM `role`',
+            "SELECT * FROM `role`",
             function(err, results,) {
               console.log(results);
             }
           );
     }
-    else if (answer.userChoice == 'View all employees') {
+    else if (answer.userChoice == "View all employees") {
         connection.query(
-            'SELECT * FROM `employee`',
+            "SELECT * FROM `employee`",
             function(err, results,) {
               console.log(results);
             }
           );
     }
-    else if (answer.userChoice == 'Add a department') {
-        
+    else if (answer.userChoice == "Add a department") {
+        connection.query(
+            "INSERT INTO `department`",
+            function(err, results,) {
+              console.log(results);
+            }
+          );
     }
-    else if (answer.userChoice == 'Add a role') {
-        
+    else if (answer.userChoice == "Add a role") {
+        connection.query(
+            "INSERT INTO `role`",
+            function(err, results,) {
+              console.log(results);
+            }
+          );
     }
-    else if (answer.userChoice == 'Add an employee') {
-        
+    else if (answer.userChoice == "Add an employee") {
+        connection.query(
+            "INSERT INTO `employee`",
+            function(err, results,) {
+              console.log(results);
+            }
+          );
     }
-    else if (answer.userChoice == 'Update an employee role') {
+    else if (answer.userChoice == "Update an employee role") {
         
     }
     
---
+++ db/seeds.sql
@@ -0,0 +1,13 @@
+INSERT INTO department (name)
+VALUES (""),
+(""),
+(""),
+("")
+
+INSERT INTO role (name, department)
+VALUES (""),
+(""),
+(""),
+("")
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
